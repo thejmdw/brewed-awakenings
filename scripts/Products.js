34c75df3b3f295d28e3a35138f812f42b11ce2e7
Jonathan Watson <thejmdw@gmail.com>
2021-04-21 21:17:43 -0400

    thejmdw added price alert

--- scripts/Products.js
+++ scripts/Products.js
@@ -1,5 +1,23 @@
 import { getProducts } from "./database.js"
 
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("product")) {
+            const [,productId] = itemClicked.id.split("--")
+
+            for (const product of products) {
+                if (product.id === parseInt(productId)) {
+                    window.alert(`${product.name} costs $${product.price}`)
+                }
+            }
+        }
+    }
+)
+
+
 const products = getProducts()
 
 export const Products = () => {
