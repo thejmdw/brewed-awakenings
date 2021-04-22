2db78265e66ce72bf3ceb3b0d75015f972c3561d
Jonathan Watson <thejmdw@gmail.com>
2021-04-21 23:03:15 -0400

    total sales for each employee

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,12 +1,37 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
+
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) { //this sorts out what is clicked
+            const [,employeeId] = itemClicked.id.split("--") //this splits the class name and sets it to a variable employeeId
+
+            let total = 0
+            const employee = employees[employeeId - 1]
+            for (const order of orders) {
+                
+                if (order.employeeId === parseInt(employeeId)) {
+                    total += 1
+                    
+                }
+                
+            }
+            window.alert(`${employee.name} sold ${total} products`)
+        }
+    }
+)
+
 
 const employees = getEmployees()
+const orders = getOrders()
 
 export const Employees = () => {
     let html = "<ul>"
 
     for (const employee of employees) {
-        html += `<li id="employee--${employee.Id}">${employee.name}</li>`
+        html += `<li id="employee--${employee.id}">${employee.name}</li>`
     }
 
     html += "</ul>"
